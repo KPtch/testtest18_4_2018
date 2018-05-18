806563fb42a25781c1dc9c928e3ccf7a4f32291f
KPtch <36377768+KPtch@users.noreply.github.com>
2018-05-18 22:31:13 +0700

    Update app.js

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ bot.dialog('/',function (session) {
 // ]);
 bot.dialog('SelectChoice',[
     function (session) {
-        builder.Prompts.choice(session, "เลือกใบที่ต้องการ", {"ใบลา","ใบขอลาออก","ใบขอลาพักการศึกษา"});
+        builder.Prompts.choice(session, "เลือกใบที่ต้องการ", "ใบลา|ใบขอลาออก|ใบขอลาพักการศึกษา");
     },
     function (session, results) {
         session.endDialog(${results.response});
